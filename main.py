aab6af3c1fc85509b8c6efa3cee10f8af4901a23
Christopher Chan <chanculture@gmail.com>
2016-05-16 14:33:04 +0800

    Implemented cronjob.  Remind user's with open games, to continue their game.

--- main.py
+++ main.py
@@ -6,32 +6,38 @@ import logging
 
 import webapp2
 from google.appengine.api import mail, app_identity
-from api import GuessANumberApi
+from api import HangmanApi
 
-from models import User
+from models import User, Game
 
 
 class SendReminderEmail(webapp2.RequestHandler):
     def get(self):
-        """Send a reminder email to each User with an email about games.
+        """Send a reminder email to each User with an email to make.
         Called every hour using a cron job"""
         app_id = app_identity.get_application_id()
-        users = User.query(User.email != None)
-        for user in users:
-            subject = 'This is a reminder!'
-            body = 'Hello {}, try out Guess A Number!'.format(user.name)
-            # This will send test emails, the arguments to send_mail are:
-            # from, to, subject, body
-            mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
-                           user.email,
-                           subject,
-                           body)
+
+        games = Game.query(Game.game_over == False)
+        for game in games:
+            user = User.query(User.key == game.user).get()
+            subject = 'Your Hangman move is waiting for you'
+            body = "Hi {}, don't forget to make your move in Hangman!\n"\
+                .format(user.name)
+            body += "The current status of your game is {}, "\
+                .format(game.get_guess_status())
+            body += "and you have {} guesses left."\
+                .format(str(game.attempts_remaining))
+            if user.email != None:
+                mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
+                                user.email,
+                                subject,
+                                body)
 
 
 class UpdateAverageMovesRemaining(webapp2.RequestHandler):
     def post(self):
         """Update game listing announcement in memcache."""
-        GuessANumberApi._cache_average_attempts()
+        HangmanApi._cache_average_attempts()
         self.response.set_status(204)
 
 
